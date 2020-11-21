fda539ffad06157829c61f3134c371cdb269421c
root <root@ALEC-PC.localdomain>
2020-11-21 17:24:04 -0500

    attempted fix to add_tlb

--- my_vm.c
+++ my_vm.c
@@ -153,8 +153,11 @@ add_TLB(void *va, void *pa)
     {
         tlb_tail->prev->next = NULL;
         tlb_count--;
-        free(tlb_tail->entry);
-        free(tlb_tail);
+        tlb *temp = tlb_tail;
+        tlb_tail = tlb_tail->prev;
+
+        free(temp->entry);
+        free(temp);
     }
 
     return -1;
